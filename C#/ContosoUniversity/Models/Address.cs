af987ba0416736f15a23be6d9a1cb16d0439d373
w13-tinfo431 <35407131+w13-tinfo431@users.noreply.github.com>
2018-03-03 21:14:56 -0800

    Initial commit

---
+++ C#/ContosoUniversity/Models/Address.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public abstract class Address
+    {
+        public int AddressId { get; set; }
+
+        [RegularExpression("@contoso.edu", ErrorMessage = "Please enter a valid E-mail address.")]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+    }
+}
---
+++ C#/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Student : Person
+    {
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+        [Display(Name = "Credits Earned")]
+        public int? CreditsEarned { get; set; }
+        [Range(0.70, 4.00)]
+        [Display(Name = "Gpa")]
+        [RegularExpression(@"^\d*.\d{0,2}$")]
+        public double? Gpa { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+      
+    }
+}
